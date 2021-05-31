a9237bee1a6938d8dfb9f7756d231bb411ff31a6
piche32 <piche32@naver.com>
2021-05-31 13:02:08 +0900

    Change Stage2 Clear Condition
    if Wizard Enemy Die, Stage 2 Clear

---
+++ Snow Fighter/Assets/Scripts/Enemy/Wizard/WizardEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardEnemy : MonoBehaviour
+{
+    Enemy.Ver2.Enemy self;
+
+    private void Start()
+    {
+        self = GetComponent<Enemy.Ver2.Enemy>();
+    }
+    private void Update()
+    {
+        if (self.checkHp())
+        {
+            GameObject enemys = GameObject.Find("Enemys");
+            int childIdx = enemys.transform.childCount;
+            for(int i = 0; i < childIdx; i++)
+            {
+                GameObject enemy = enemys.transform.GetChild(i).gameObject;
+                if (enemy != null && enemy.activeSelf)
+                {
+                    if (enemy.name == "WizardEnemy") continue;
+                    Enemy.Ver2.Enemy enemyScript = enemy.GetComponent<Enemy.Ver2.Enemy>();
+                    if (enemyScript != null)
+                    {
+                        enemyScript.Hit(enemyScript.HP + 1.0f);
+                    }
+                }
+            }
+        }
+    }
+}
